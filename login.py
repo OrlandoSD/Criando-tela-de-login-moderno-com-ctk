33684be8f00b45f9235ec3cb03e2e93bec3b0724
OrlandoSD <py2osd.orlando@gmail.com>
2024-06-09 14:34:51 -0300

    fcommit finalizando tela de login e começando tela de cadastro

--- login.py
+++ login.py
@@ -1,45 +1,66 @@
 from tkinter import PhotoImage, RIGHT
 from turtle import right
-import customtkinter
+import customtkinter as ctk
+
+janela = ctk.CTk()
+
+class Application():
+    def __init__(self):
+        self.janela=janela
+        self.tema()
+        self.tela()
+        self.janela_login()
+        janela.mainloop()
 
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
+    def tema(self):
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("dark-blue")
 
-janela = customtkinter.CTk()
-janela.geometry("700x400")
-janela.title("Sistema de login")
-janela.iconbitmap("icon.ico")
-janela.resizable(False, False)
+    def tela(self):
+        self.janela.geometry("700x400")
+        self.janela.title("Sistema de login")
+        self.janela.iconbitmap("login.ico")
+        self.janela.resizable(False, False)
+
+    def janela_login(self):
 
 
-img = PhotoImage(file="login.png")
-label_img = customtkinter.CTkLabel(master=janela, image=img)
-label_img.place(x=25, y=65)
+        #Trabalando com a imagem da tela
+        img = PhotoImage(file="login.png")
+        label_img = ctk.CTkLabel(master=janela, image=img, text=None)
+        label_img.place(x=25, y=65)
 
-label_tt = customtkinter.CTkLabel(master=janela, text="Entrar com seu login e senha", font=("Roboto", 18), text_color="white")
-label_tt.place(x=60, y=10)
+        title_label = ctk.CTkLabel(master=janela, text="Entrar com seu login e senha", font=("Roboto", 18), text_color="white")
+        title_label.place(x=60, y=10)
 
-frame = customtkinter.CTkFrame(master=janela, width=350, height=396)
-frame.pack(side=RIGHT)
+        #frame
+        login_frame = ctk.CTkFrame(master=janela, width=350, height=396)
+        login_frame.pack(side=RIGHT)
 
-label = customtkinter.CTkLabel(master=frame, text="Sistema de Login", font=("Roboto", 20))
-label.place(x=35, y=5)
+        #widgest dentro da frame de tela de login
+        label = ctk.CTkLabel(master=login_frame, text="Sistema de Login", font=("Roboto", 20))
+        label.place(x=35, y=5)
 
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Nome de usuário", width=300, font=("Roboto", 14))
-entry1.place(x=25, y=105)
+        Username_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Nome de usuário", width=300, font=("Roboto", 14)).place(x=25, y=105)
 
-label1 = customtkinter.CTkLabel(master=frame, text="O campo usuário é de caráter obrigatório!", text_color="green", font=("Roboto", 12))
-label1.place(x=25, y=135)
+        username_label = ctk.CTkLabel(master=login_frame, text="O campo usuário é de caráter obrigatório!", text_color="green", font=("Roboto", 12))
+        username_label.place(x=25, y=135)
 
-entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Senha de usuário", width=300, font=("Roboto", 14))
-entry2.place(x=25, y=175)
+        password_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Senha de usuário", width=300, font=("Roboto", 14), show="*").place(x=25, y=175)
 
-label2 = customtkinter.CTkLabel(master=frame, text="O campo Senha é de caráter obrigatório!", text_color="green", font=("Roboto", 12))
-label2.place(x=25, y=205)
+        password_label = ctk.CTkLabel(master=login_frame, text="O campo Senha é de caráter obrigatório!", text_color="green", font=("Roboto", 12))
+        password_label.place(x=25, y=205)
 
-checkbox = customtkinter.CTkCheckBox(master=frame, text="Lembrar-se do login sempre").place(x=25, y=235)
+        checkbox = ctk.CTkCheckBox(master=login_frame, text="Lembrar-se do login sempre").place(x=25, y=235)
 
-button = customtkinter.CTkButton(master=frame, text="LOGIN", width=300).place(x=25, y=285)
+        login_button = ctk.CTkButton(master=login_frame, text="LOGIN", width=300).place(x=25, y=285)
 
-janela.mainloop()
+        def tela_register():
+            #remover o frame de login
+            pass
+        register_span = ctk.CTkLabel(master=login_frame, text="se não tem uma conta").place(x=25, y=325)
+        register_button = ctk.CTkButton(master=login_frame, text="Cadastre-se", width=150, fg_color="green", hover_color="#2D9334", command=tela_register).place(x=175, y=325)
+
+
+Application()
